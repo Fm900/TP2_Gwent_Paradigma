b652d0026f5c7f3702289cee5f402628e80e28a0
Juan Claros <jclaros@fi.uba.ar>
2025-06-02 10:27:27 -0300

    #5 first solution - the first version is added to the test of moving a card to the discard pile

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/PlayerDiscardPileTest.java
@@ -0,0 +1,33 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerDiscardPileTest {
+
+    private Player player;
+    private Card card1;
+
+    @BeforeEach
+    void setUp() {
+        card1 = new Card("Warrior", 5);
+        Deck deck = new Deck();
+        deck.addCard(card1);
+
+        Hand hand = new Hand();
+        DiscardPile discardPile = new DiscardPile();
+
+        player = new Player(deck, hand, discardPile);
+        player.drawCard();
+    }
+
+    @Test
+    void testCardGoesToDiscardPile() {
+        player.playCard(0);
+        assertEquals(1, player.getDiscardPile().getCards().size());
+        assertEquals("Warrior", player.getDiscardPile().getCards().get(0).getName());
+    }
+}
